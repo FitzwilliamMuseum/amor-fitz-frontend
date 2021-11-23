5f5bfac576e7a8d0a5247b697607d08dfbbb18e5
Daniel Pett <dejp3@cam.ac.uk>
2021-11-23 14:51:46 +0000

    Update welcome layout

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -10,21 +10,32 @@
     <h1 class="f3 f2-m f1-l serif">{{ $page['title']}}</h1>
     {!! $page['text'] !!}
   </article>
-  <section class="mw9 mw9-ns center bg-creme pa2 ph5-ns">
-    <cta-card
-    title="Correspondences"
-    content="Explore the world of Hayley's correspondence"
-    button-text="Start reading"
-    button-link="{{ route('correspondences') }}"
-    />
-  </section>
 
-  <section class="bg-creme pa2 ph5-ns">
-    <cta-card
-    title="People, Places, and more"
-    content="Explore the entities of Hayley's world"
-    button-text="Explore entities"
-    button-link="{{ route('entities') }}"
-    />
-  </section>
+
+  <section class="cf ph3 pa2 ph5-ns  bg-light-green black-70" id="features">
+            <div class="mw9 center">
+
+              <div class="cf">
+                <article class="pv2 fl w-100 w-50-l pr0 pr2-l">
+                  <cta-card
+                  title="People, Places, and more"
+                  content="Explore the entities of Hayley's world"
+                  button-text="Explore entities"
+                  button-link="{{ route('entities') }}"
+                  />
+                </article>
+                <article class="pv2 fl w-100 w-50-l pl0 pl2-l">
+                  <cta-card
+                  title="Correspondences"
+                  content="Explore the world of Hayley's correspondence"
+                  button-text="Start reading"
+                  button-link="{{ route('correspondences') }}"
+                  />
+                </article>
+              </div>
+
+            </div>
+          </section>
+
+  
 @endsection
