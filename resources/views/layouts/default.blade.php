d64ce16638b77559dbc63f8123d7e1d8ac62cb0c
Daniel Pett <dejp3@cam.ac.uk>
2021-11-23 13:19:44 +0000

    Update layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,26 +3,19 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <title>
-    @hasSection('title')
-      @yield('title') |
-    @endif
-    {{ config('app.name') }}</title>
-    <link rel="stylesheet" href="{{ mix('/css/fitzwilliam.css') }}" />
-    <link rel="stylesheet" href="{{ mix('/css/global-styles.css') }}" />
-  </head>
-  <body class="w-100"  >
-    <div class="" id="app">
-      <the-header></the-header>
-      <section class="mw9 mw9-ns center bg-white pa3 ph5-ns">
-        {{-- <breadcrumbs /> --}}
-        @yield('breadcrumbs')
-      </section>
-
-      @yield('content')
-    </div>
-    @include('includes.footer')
-
-    </body>
-    </html>
+    @hasSection('title') @yield('title') | @endif {{ config('app.name') }}</title>
+  <link rel="stylesheet" href="{{ mix('/css/fitzwilliam.css') }}" />
+  <link rel="stylesheet" href="{{ mix('/css/global-styles.css') }}" />
+</head>
+<body class="w-100"  >
+  <div class="" id="app">
+    <the-header></the-header>
+    <section class="mw9 mw9-ns center pa3 ph5-ns">
+      @yield('breadcrumbs')
+    </section>
+    @yield('content')
+  </div>
+  @include('includes.footer')
+</body>
+</html>
