97f9b204707432b0645175e99da792be9f85e478
Daniel Pett <dejp3@cam.ac.uk>
2021-11-25 21:35:04 +0000

    Add social tags

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,47 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>
-    @hasSection('title') @yield('title') | @endif {{ config('app.name') }}</title>
+    @hasSection('title') @yield('title') | @endif {{ config('app.name') }}
+  </title>
+  <meta name="keywords" content="@yield('keywords','relationships,letters,correspondence')">
+  <meta name="description" content="@yield('description','A Museum of Relationships, a digital edition of William Hayley\'s correspondence')">
+<!-- Open graph -->
+  <meta property="og:locale" content="{{ app()->getLocale() }}" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="@yield('title')" />
+  <meta property="og:description" content="@yield('description','A Museum of Relationships, a digital edition of William Hayley\'s correspondence')" />
+  <meta property="og:url" content="{{ URL::current() }}" />
+  <meta property="og:site_name" content="The Fitzwilliam Museum" />
+
+  @hasSection('socialImage')
+  <meta property="og:image" content="@yield('socialImage')" />
+  <meta name="twitter:image" content="@yield('socialImage')" />
+  @else
+  <meta property="og:image" content="/images/hayley.jpg" />
+  <meta name="twitter:image" content="/images/hayley.jpg" />
+  @endif
+
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <!-- Twitter card -->
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:description" content="@yield('description','A Museum of Relationships, a digital edition of William Hayley\'s correspondence')" />
+  <meta name="twitter:title" content="@yield('title')" />
+  <meta name="twitter:site" content="@yield('twitter_id', '@fitzmuseum_uk')" />
+  <meta name="twitter:creator" content="@yield('twitter_id', '@fitzmuseum_uk')" />
+  <link rel="canonical" href="{{url()->current()}}"/>
   <link rel="stylesheet" href="{{ mix('/css/fitzwilliam.css') }}" />
   <link rel="stylesheet" href="{{ mix('/css/global-styles.css') }}" />
+  <link rel="icon" href="/images/favicon.ico">
+  <script type="application/ld+json">
+      {"publisher":{"@type":"Organization",
+      "logo":{"@type":"ImageObject",
+      "url":"https://fitzmuseum.cam.ac.uk/images/logos/FV.png"}},
+      "headline":"A museum of relationships","@type":"WebSite","url":"{{ route('home')}}
+      "name":"The Fitzwilliam Museum",
+      "description":"The Fitzwilliam Museum is the principal museum of the University of Cambridge",
+      "@context":"https://schema.org"}
+    </script>
 </head>
 <body class="w-100 bg-white">
   <div class="" id="app">
@@ -15,7 +53,7 @@
       @yield('breadcrumbs')
     </section>
     <main class="w-100 ">
-    @yield('content')
+      @yield('content')
     </main>
   </div>
   @include('includes.footer')
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 @section('breadcrumbs')
-<breadcrumbs
-    :path-list='[]'
+  <breadcrumbs
+  :path-list='[]'
   />
 @endsection
 @section('title', 'Welcome')
@@ -11,31 +11,27 @@
     {!! $page['text'] !!}
   </article>
 
-
   <section class="cf ph3 pa2 ph5-ns  bg-light-green black-70" id="features">
-            <div class="mw9 center">
+    <div class="mw9 center">
+      <div class="cf">
+        <article class="pv2 fl w-100 w-50-l pr0 pr2-l">
+          <cta-card
+          title="People, Places, and more"
+          content="Explore the entities of Hayley's world"
+          button-text="Explore entities"
+          button-link="{{ route('entities') }}"
+          />
+        </article>
+        <article class="pv2 fl w-100 w-50-l pl0 pl2-l">
+          <cta-card
+          title="Correspondences"
+          content="Explore the world of Hayley's correspondence"
+          button-text="Start reading"
+          button-link="{{ route('correspondences') }}"
+          />
+        </article>
+      </div>
 
-              <div class="cf">
-                <article class="pv2 fl w-100 w-50-l pr0 pr2-l">
-                  <cta-card
-                  title="People, Places, and more"
-                  content="Explore the entities of Hayley's world"
-                  button-text="Explore entities"
-                  button-link="{{ route('entities') }}"
-                  />
-                </article>
-                <article class="pv2 fl w-100 w-50-l pl0 pl2-l">
-                  <cta-card
-                  title="Correspondences"
-                  content="Explore the world of Hayley's correspondence"
-                  button-text="Start reading"
-                  button-link="{{ route('correspondences') }}"
-                  />
-                </article>
-              </div>
-
-            </div>
-          </section>
-
-  
+    </div>
+  </section>
 @endsection
