9e1562dc7ab05cf4f1d68f11ddec6618b84ca8ff
Daniel Pett <dejp3@cam.ac.uk>
2021-11-25 00:00:05 +0000

    Update scripts

--- resources/views/entities/index.blade.php
+++ resources/views/entities/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.default')
 @section('breadcrumbs')
-<breadcrumbs
-    :path-list='[
-    {"text":"Entities","path":"{{ route('entities')}}"}
-    ]'
+  <breadcrumbs
+  :path-list='[
+  {"text":"Entities","path":"{{ route('entities')}}"}
+  ]'
   />
 @endsection
 @section('title',  $page['title'])
@@ -17,57 +17,57 @@
 
 
   <section class="cf ph3 ph7-ns pb1 bg-light-green black-70" id="features">
-              <div class="mw9 center">
+    <div class="mw9 center">
 
-                <div class="cf">
-                  <article class="pv2 fl w-100 w-50-l pr0 pr2-l">
-                    <entity-list-card
-                    :names="['People']"
-                    :backgrounds="['blake']"
-                    :numberletters='{{$people[0]['items']['count']}}'
-                    button-text="Discover colourful characters"
-                    button-link="{{ route('entity', ['person']) }}"
-                    />
-                  </article>
-                  <article class="pv2 fl w-100 w-50-l pl0 pl2-l">
-                    <entity-list-card
-                    :names="['Locations']"
-                    :backgrounds="['blake']"
-                    :numberletters='{{$places[0]['items']['count']}}'
-                    button-text="Visit disparate locations"
-                    button-link="{{ route('entity', ['place']) }}"
-                    />
-                  </article>
-                </div>
+      <div class="cf">
+        <article class="pv2 fl w-100 w-50-l pr0 pr2-l">
+          <entity-list-card
+          :names="['People']"
+          :backgrounds="['blake']"
+          :numberletters='{{$people[0]['items']['count']}}'
+          button-text="Discover colourful characters"
+          button-link="{{ route('entity', ['person']) }}"
+          />
+        </article>
+        <article class="pv2 fl w-100 w-50-l pl0 pl2-l">
+          <entity-list-card
+          :names="['Locations']"
+          :backgrounds="['blake']"
+          :numberletters='{{$places[0]['items']['count']}}'
+          button-text="Visit disparate locations"
+          button-link="{{ route('entity', ['place']) }}"
+          />
+        </article>
+      </div>
 
-                <div class="cf">
-                  <article class="pv2 fl w-100 w-50-l pr0 pr2-l">
-                    <entity-list-card
-                    :names="['Families']"
-                    :backgrounds="['blake']"
-                    :numberletters='{{$family[0]['items']['count']}}'
-                    button-text="Uncover family connections"
-                    button-link="{{ route('entity', ['family']) }}"
-                    />
-                  </article>
-                  <article class="pv2 fl w-100 w-50-l pl0 pl2-l">
-                    <entity-list-card
-                      :names="['Events mentioned']"
-                      :backgrounds="['blake']"
-                      :numberletters='{{$events[0]['items']['count']}}'
-                      button-text="Relive events"
-                      button-link="{{ route('entity', ['event']) }}"
-                    />
-                  </article>
-                </div>
+      <div class="cf">
+        <article class="pv2 fl w-100 w-50-l pr0 pr2-l">
+          <entity-list-card
+          :names="['Families']"
+          :backgrounds="['blake']"
+          :numberletters='{{$family[0]['items']['count']}}'
+          button-text="Uncover family connections"
+          button-link="{{ route('entity', ['family']) }}"
+          />
+        </article>
+        <article class="pv2 fl w-100 w-50-l pl0 pl2-l">
+          <entity-list-card
+          :names="['Events mentioned']"
+          :backgrounds="['blake']"
+          :numberletters='{{$events[0]['items']['count']}}'
+          button-text="Relive events"
+          button-link="{{ route('entity', ['event']) }}"
+          />
+        </article>
+      </div>
 
-              </div>
-            </section>
-
-
+    </div>
+  </section>
 
 
 
 
 
-    @endsection
+
+
+@endsection
--- resources/views/includes/iiif.blade.php
+++ resources/views/includes/iiif.blade.php
@@ -1,3 +1,7 @@
 <section class="w-100">
-<div class="uv" data-locale="en-GB:English (GB),cy-GB:Cymraeg" data-config="/plugins/UniversalViewer/views/public/universal-viewer/config.json" data-uri="https://hayleypapers.fitzmuseum.cam.ac.uk/iiif/{{ $data[0]['id'] }}/manifest" data-collectionindex="0" data-manifestindex="0" data-sequenceindex="0" data-canvasindex="0" data-xywh="-2071,-341,9278,6804" data-rotation="0" style="width:100%; height:600px; background-color: #000"></div>
+  <div class="uv" data-locale="en-GB:English (GB),cy-GB:Cymraeg" data-config="/plugins/UniversalViewer/views/public/universal-viewer/config.json" data-uri="https://hayleypapers.fitzmuseum.cam.ac.uk/iiif/{{ $data[0]['id'] }}/manifest" data-collectionindex="0" data-manifestindex="0" data-sequenceindex="0" data-canvasindex="0" data-xywh="-2071,-341,9278,6804" data-rotation="0" style="width:100%; height:600px; background-color: #000"></div>
 </section>
+@section('scripts')
+  <script type="text/javascript" id="embedUV" src="https://hayleypapers.fitzmuseum.cam.ac.uk/plugins/UniversalViewer/views/shared/javascripts/uv/lib/embed.js"></script>
+  <script type="text/javascript">/* wordpress fix */</script>
+@endsection
