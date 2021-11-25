2b2f3e67750ddf52a368a5dc9c9ccded1efc8bd3
Daniel Pett <dejp3@cam.ac.uk>
2021-11-25 16:57:32 +0000

    Fix errors

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.default')
-@section('title', 'Page not found')
+@section('title', 'Internal server error')
 
 @endsection
 @section('breadcrumbs')
@@ -13,8 +13,8 @@
 
   <section class="vh-80 bg-creme baskerville">
   <header class="tc ph5 lh-copy">
-      <h1 class="f1 f-headline-l code mb3 fw9 dib tracked-tight light-purple">404</h1>
-      <h2 class="tc f1-l fw1">Sorry, we can't find the page you are looking for.</h2>
+      <h1 class="f1 f-headline-l code mb3 fw9 dib tracked-tight light-purple">500</h1>
+      <h2 class="tc f1-l fw1">Sorry, our code or server has a problem.</h2>
   </header>
 
 </section>
