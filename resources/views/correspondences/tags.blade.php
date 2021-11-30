394917ac2daa3f8822954407b64a671e8136640f
Daniel Pett <dejp3@cam.ac.uk>
2021-11-30 11:55:15 +0000

    Remove waste of space text

--- resources/views/correspondences/tags.blade.php
+++ resources/views/correspondences/tags.blade.php
@@ -18,7 +18,6 @@
 <article class="mw9 mw9-ns bg-white pa3 ph5-ns">
   @if(!empty($tags))
     <div class="ph3">
-      <h2 class="helvetica f4">Tags</h2>
       <tag-list
       :tags='[
       @foreach($tags as $tag)
