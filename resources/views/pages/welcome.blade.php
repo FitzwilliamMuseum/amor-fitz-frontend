d989221338de054bb40bc92af6628964d1136050
Daniel Pett <dejp3@cam.ac.uk>
2021-12-02 17:31:48 +0000

    Drop buttons

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -19,26 +19,4 @@
     <div class="pa1 pa5-ns">{!! $page['text'] !!}</div>
   </article>
 
-  <section class="cf ph3 pa4 ph7-ns bg-light-green black-70" id="features">
-    <div class="mw9 center">
-      <div class="cf">
-        <article class="pv2 fl w-100 w-50-l pr0 pr2-l">
-          <cta-card
-          title="People & Places"
-          content="Explore the entities of Hayley's world"
-          button-text="Explore entities"
-          button-link="{{ route('entities') }}"
-          />
-        </article>
-        <article class="pv2 fl w-100 w-50-l pl0 pl2-l">
-          <cta-card
-          title="Correspondences"
-          content="Explore the world of Hayley's correspondence"
-          button-text="Start reading"
-          button-link="{{ route('correspondences') }}"
-          />
-        </article>
-      </div>
-    </div>
-  </section>
 @endsection
