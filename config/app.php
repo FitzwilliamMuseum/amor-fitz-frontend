56821f7708b58220363ac8f6fc5f528d37f18800
Daniel Pett <dejp3@cam.ac.uk>
2022-07-23 00:54:09 +0100

    Update NPM packages Update to laravel 9.21

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Buzzylab\Laradown\MarkdownServiceProvider::class,
 
 
     ],
@@ -231,9 +230,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Markdown' => Buzzylab\Laradown\Facades\MarkdownFacade::class
-
-
     ],
 
 ];
