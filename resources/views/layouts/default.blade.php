bdd3a57109c730fb5f64ac21cc86f0f67f7b8f70
Gareth Trinkwon <gareth@studio24.net>
2025-08-15 11:13:50 +0100

    Removed GTM and GA code from layout.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -65,9 +65,6 @@
   </div>
   @include('includes.footer')
   @yield('scripts')
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-8H7LKK1CKC"></script>
-
-  <script src="/js/ga.js" type="text/javascript"></script>
 
 </body>
 </html>
